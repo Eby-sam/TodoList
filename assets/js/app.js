ec7f7cfe569119c8ca2ba75e43045d7af9d7c96d
coque <coquelet.samuel@gmail.com>
2022-11-16 12:30:51 +0100

    debut de js

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+let insert = document.getElementById('insert');
+let puching = document.getElementById('puching');
+let clearItems = document.getElementById('clear');
+let list = [];
+
+function addList() {
+
+}
+
+addList.addEventListener('click',puching);
